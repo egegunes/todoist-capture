89f16a150a5323a3f0e01ec7d2c8c6464c944eaa
Ege Güneş <egegunes@gmail.com>
2018-12-04 23:19:19 +0300

    Fix installation with pip

--- setup.py
+++ setup.py
@@ -6,12 +6,9 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-with open('requirements.txt') as f:
-    requirements = f.read()
-
 setup(
     name='todoist-capture',
-    version='1.0.2',
+    version='1.0.3',
     description='Capture tasks to your Inbox from command line',
     long_description=readme,
     long_description_content_type='text/markdown',
@@ -20,7 +17,9 @@ setup(
     license="GPLv3",
     url='https://github.com/egegunes/todoist-capture',
     packages=find_packages(exclude=('tests', 'docs')),
-    install_requires=requirements,
+    install_requires=[
+        "todoist-python==7.0.18"
+    ],
     entry_points={
         'console_scripts': ['capture=capture.capture:run']
     }
